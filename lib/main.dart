accd8a6cc5df092c93d479a05f49246809322ec1
Márcio <marcio.quimbundo@ucall.co.ao>
2019-01-10 15:09:11 +0100

    bottomNavigationBar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Lombongo());
+void main() => runApp(Home());
 
-class Lombongo extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Lombongo'),
+          title: Text('Lombongo App'),
+          backgroundColor: Colors.green,
+          centerTitle: false,
+          elevation: 2.0,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            )
+          ],
         ),
-        body: Container(
-
+        bottomNavigationBar: Container(
+          color: Colors.white,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ListTile(
+                  title: Text('Balanço: '),
+                  subtitle: Text("\$720"),
+                ),
+              ),
+              Expanded(
+                child: ListTile(
+                  title: Text('Despesas: '),
+                  subtitle: Text("\$230", style: TextStyle(color: Colors.red)),
+                ),
+              ),
+              Expanded(
+                child: IconButton(
+                  icon: Icon(Icons.remove_red_eye, color: Colors.green),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
         ),
+        body: Container(),
       ),
     );
   }
