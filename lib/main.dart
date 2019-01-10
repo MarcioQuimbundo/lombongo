2b8c26db2b164aebf73690df158fa464e0d90f29
Márcio <marcio.quimbundo@ucall.co.ao>
2019-01-10 15:39:19 +0100

    alert dialog added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Home());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ));
 
 class Home extends StatefulWidget {
   @override
@@ -12,48 +15,64 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Lombongo App'),
-          backgroundColor: Colors.green,
-          centerTitle: false,
-          elevation: 2.0,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {},
-            )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lombongo App'),
+        backgroundColor: Colors.green,
+        centerTitle: false,
+        elevation: 2.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              //this will call the alert
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        title: Text("Add"),
+                        content: Container(
+                          height: 100.0,
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text('teste'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ));
+            },
+          )
+        ],
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: ListTile(
+                title: Text('Balanço: '),
+                subtitle: Text("\$720"),
+              ),
+            ),
+            Expanded(
+              child: ListTile(
+                title: Text('Despesas: '),
+                subtitle: Text("\$230", style: TextStyle(color: Colors.red)),
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.remove_red_eye, color: Colors.green),
+                onPressed: () {},
+              ),
+            ),
           ],
         ),
-        bottomNavigationBar: Container(
-          color: Colors.white,
-          child: Row(
-            children: <Widget>[
-              Expanded(
-                child: ListTile(
-                  title: Text('Balanço: '),
-                  subtitle: Text("\$720"),
-                ),
-              ),
-              Expanded(
-                child: ListTile(
-                  title: Text('Despesas: '),
-                  subtitle: Text("\$230", style: TextStyle(color: Colors.red)),
-                ),
-              ),
-              Expanded(
-                child: IconButton(
-                  icon: Icon(Icons.remove_red_eye, color: Colors.green),
-                  onPressed: () {},
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: Container(),
       ),
+      body: Container(),
     );
   }
 }
